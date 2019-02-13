a0309b10acdead6b4be48c4743ac0c3e5ce1ef43
OoDEGPoO <Oo.DEGP.oO@gmail.com>
2019-02-13 11:28:02 +0100

    Mejoras y añadidos
    Añadidas pruebas de testeo con matrices 6x6
    
    Añadida función para el cálculo del Grid de Bloques, el numero de hilos totales debe ser mayor que la matriz, por lo que se redondea a la alza el cálculo
    
    Añadido filtro de hilos en la ejecución de estos para que los hilos extras no actuen sobre lo que no deben
    
    Se han comprobado los cálculos con varios tipos de matriz y los resultados han sido correctos

--- ProductoMatricialCuda/kernel.cu
+++ ProductoMatricialCuda/kernel.cu
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
 cudaError_t prodMatricesCuda(int *c, const int *a, const int *b, unsigned int Width);
 const int TILE_WIDTH = 4;//Se ha establecido un tamaño de tesela de 4 hilos
@@ -17,10 +18,20 @@ __global__ void productoKernel(int *c, const int *a, const int *b, unsigned int 
 	int id_col = blockIdx.x * TILE_WIDTH + threadIdx.x;
 	int n = 0;
 
-	for (int i = 0; i < Width; i++) {
-		n = n + (a[id_fil*Width + i] * b[i*Width + id_col]);
+	if ((id_fil < Width) && (id_col < Width)) {//Si el hilo está fuera de los valores, no debe actuar
+		for (int i = 0; i < Width; i++) {
+			n = n + (a[id_fil*Width + i] * b[i*Width + id_col]);
+		}
+		c[id_fil*Width + id_col] = n;
 	}
-    c[id_fil*Width + id_col] = n;
+}
+
+//Hace uso de ceil para obtener el tamaño del Grid de Bloques
+int grid_calc(int Width, int Tile_Width) {
+	double x =  Width;
+	double y = Tile_Width;
+
+	return (int)(ceil(x / y));//redondea hacia arriba la división
 }
 
 void imprimeMatriz(int *v, int m, int n) {//( m * n )
@@ -107,9 +118,33 @@ void generaMatriz(int *v, int m, int max, int min) {//Para matrices cuadradas ( 
 
 int main()
 {
-	srand(time(0));
+	/*const int Width = 6;//Pruebas
+	int a[6 * 6] = {	8, 7, 3, -4, -2, -3,
+						8, 9, -6, 3, -1, -4,
+						-6, -1, -10, -7, 8, 6,
+						4, -6, -6, -3, 8, 7,
+						-1, -1, -7, -8, -1, 9,
+						7, 8, 3, 7, 2, 3 };
+
+	int b[6 * 6] = {	0, 9, 9, 5, -10, 6,
+						-2, 3, 8, 4, 0, -9,
+						7, 1, -8, -9, -10, -9,
+						-3, -5, 7, -2, 6, 4,
+						7, 9, -3, -9, -9, 6,
+						6, 6, 4, -8, 8, -5 };
+
+	//Resultado de a * b =
+	//		-13		80		70		91		-140	-55
+	//		-100	45		200		165		-25		47
+	//		45		76		-31		-50		94		53
+	//		77		141		19		-72		-14		133
+	//		24		66		22		7		113		-17
+	//		16		91		158		-16		-52		-32*/
+
+	srand((unsigned int)time(0));
 	const int max = 10;
 	const int min = -10;
+
     const int Width = 16;
 	int a[Width * Width] = { 0 };
 	generaMatriz(a, Width, max, min);
@@ -128,9 +163,9 @@ int main()
 
 	printf("\n\tMatriz A\n");
 	imprimeMatriz(a, Width);
-	printf("\n\tProducto de Matriz B\n");
+	printf("\n\tMatriz B\n");
 	imprimeMatriz(b, Width);
-	printf("\n\tResultado:\n");
+	printf("\n\tResultado del producto:\n");
 	imprimeMatriz(c, Width);
 	
     // cudaDeviceReset must be called before exiting in order for profiling and
@@ -150,7 +185,7 @@ cudaError_t prodMatricesCuda(int *c, const int *a, const int *b, unsigned int Wi
     int *dev_a = 0;
     int *dev_b = 0;
     int *dev_c = 0;
-	dim3 DimGrid(Width/TILE_WIDTH, Width / TILE_WIDTH);
+	dim3 DimGrid(grid_calc(Width, TILE_WIDTH), grid_calc(Width, TILE_WIDTH));
 	dim3 DimBlock(TILE_WIDTH, TILE_WIDTH);
     cudaError_t cudaStatus;
 
